72f28488c07d4f6586ebb1d3f8e013a2247bb8f1
JacobTheProgrammer <jacobtheperson123@hotmail.com>
2018-10-26 17:31:24 +0800

    update

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,11 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$imgUrls = [
+define("IMG_URLS", [
     "http://www.youxigt.com/d/file/2018-09-13/f02889fb3fec4e7ddb8fd9e00b6b781e.jpg",
     "https://i.ytimg.com/vi/OZkgIc4bU1I/maxresdefault.jpg",
     "https://strengthnet.com/wp-content/uploads/2016/12/Larry-Williams-Headshot.jpg"
-];
+]);
 
 $factory->define(App\Article::class, function (Faker $faker) {
     return [
@@ -14,7 +14,7 @@ $factory->define(App\Article::class, function (Faker $faker) {
         'body' => $faker->text(200),
         'author' => $faker->text(10),
         'editor' => $faker->text(10),
-        'img_urls' => "[".$imgUrls[rand(0, sizeof($imgUrls) - 1)]."]",
+        'img_urls' => "[".IMG_URLS[rand(0, sizeof(IMG_URLS) - 1)]."]",
         'view_count' => 0
     ];
 });
